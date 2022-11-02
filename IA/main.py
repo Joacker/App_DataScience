4f754abdbd72e37c71b1c90d09dc9e17ebb915c5
Joaquín Fernández <67400456+Joacker@users.noreply.github.com>
2022-11-02 19:14:44 -0300

    new commit

--- IA/main.py
+++ IA/main.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-message = ''
+message = 'asdasd'
+
+@app.route('/api', methods=['GET'])
+def ping():
+    return jsonify({'message': message})
 
 if __name__== "__main__":
-    app.run(debug = True)
+    app.run(host='0.0.0.0', port=80, debug = True, threaded = True)
