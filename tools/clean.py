9a2c3bb989020d3977c31c295bd6a7ec7af3bd2a
Joaquín Fernández <67400456+Joacker@users.noreply.github.com>
2022-11-21 13:01:15 -0300

    month normalize extract

--- tools/clean.py
+++ tools/clean.py
@@ -6,15 +6,23 @@ PATH = './clean/2018_Columna.csv'
 
 if __name__ == "__main__":
     data = pd.read_csv(PATH, delimiter=';')
-    # print(data.loc[:,'Fecha'])
+    print((data.loc[:,'Fecha'][0]).split("/"))
+    for i in range(len(data)):
+        data.loc[i,'Fecha'] = (data.loc[i,'Fecha']).split("/")[1]
+    print((data.loc[:,'Fecha']))
     
+    data['Encoded_mes'] = (data.loc[:,'Fecha'])
     le = LabelEncoder()
     data['Encoded_comuna'] = le.fit_transform(data['Comuna'])
 
     #print(data['Encoded_comuna'])
     cod = le.classes_
 
-    data.to_csv('../ia/csv_normalize_2018.csv')
+    data.to_csv('../ia/csv_normalize_2018v2.csv')
+    #print(data['Encoded_mes'])
+    #cod = le.classes_
+
+    #data.to_csv('../ia/csv_normalize_2018.csv')
 
     
 # le = LabelEncoder()
