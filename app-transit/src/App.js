50a821fa95db445d862524b05c76b03ef0935813
Joaquín Fernández <67400456+Joacker@users.noreply.github.com>
2022-12-16 23:56:40 -0300

    cli start

--- app-transit/src/App.js
+++ app-transit/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
 import './App.css';
-
-import MapView from './components/MapView.js'
+import Navbar from './components/sidebar/Navbar';
+import { BrowserRouter as Router,
+         Routes,
+         Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Stats from './pages/Stats';
+import Config from './pages/Config';
 
 function App() {
   return (
-   <MapView/>
+    <>
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact component={Home}/>
+        <Route path="/reports" component={Reports} />
+        <Route path="/stats" component={Stats} />
+        <Route path="/config" component={Config} />
+      </Routes>
+      </Router>
+    </>
   );
 }
 
